0d9c7d08740bf8b4e1e0a070b75aa59c2f4e5395
leoval1999 <leonel.valenzuela99@gmail.com>
2022-05-26 00:05:00 -0300

    levantando heroku

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 const path = require("path")
 const publicPath = path.join(__dirname,"/public");
+const port = process.env.PORT || 3000;
 
-app.listen(3000,()=>console.log("Funcionando 3000"));
+app.listen(port,()=>console.log('Funcionando ${port}'));
 app.use(express.static(publicPath));
 
 app.get("/",(req,res)=>{res.sendFile(path.join(__dirname,"views/home.html"))})
