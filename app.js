5a385366312335983ee8bd200b4d2e19b4d58b76
leoval1999 <leonel.valenzuela99@gmail.com>
2022-05-26 19:20:29 -0300

    feat: subiendo vistas home,carrito,detalle,ingreso,registro

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const express = require("express");
 const app = express();
 const path = require("path")
 const publicPath = path.join(__dirname,"/public");
-const port = process.env.PORT || 3000;
 
-app.listen(port,()=>console.log('Funcionando ${port}'));
+app.listen(3000,()=>console.log("Funcionando 3000"));
 app.use(express.static(publicPath));
 
 app.get("/",(req,res)=>{res.sendFile(path.join(__dirname,"views/home.html"))})
@@ -14,3 +13,4 @@ app.get("/registro",(req,res)=>{res.sendFile(path.join(__dirname,"views/registro
 app.get("/ingreso",(req,res)=>{res.sendFile(path.join(__dirname,"views/ingreso.html"))})
 app.get("/buscador",(req,res)=>{res.sendFile(path.join(__dirname,"views/buscador.html"))})
 app.get("/carrito-compras",(req,res)=>{res.sendFile(path.join(__dirname,"views/carrito.html"))})
+app.get("/detalle",(req,res)=>{res.sendFile(path.join(__dirname,"views/detalle.html"))})
