d946c9358c4082657fe1a7c9bfde7cc01dbc938d
leoval1999 <leonel.valenzuela99@gmail.com>
2022-06-08 20:32:18 -0300

    feat: modificando estructura de carpetas ejs

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path")
 const publicPath = path.join(__dirname,"/public");
-const homeRoute = require("./routes/mainRoutes");
+const homeRoute = require("./src/routes/mainRoutes");
 const port = process.env.PORT || 3000;
 
 
---
+++ src/controllers/homeController.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+const homeController ={
+    inicio: (req,res)=>{ res.render('/products/home')},
+    citas: (req,res)=>{res.render('./products/citas')},
+    petshop: (req,res)=>{res.render('./products/petshop')},
+    registro: (req,res)=>{res.render('./users/registro')},
+    ingreso: (req,res)=>{res.render('./users/ingreso')},
+    carritoCompras: (req,res)=>{res.render('./products/carrito')},
+    detalle: (req,res)=>{res.render('./products/detalle')}
+}
+module.exports = homeController;
